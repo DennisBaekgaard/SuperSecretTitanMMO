8d9a76384085292a5c8e96a531d22f2c8f766836
Stefan Micheelsen <stufkan@gmail.com>
2012-03-26 22:32:49 +0200

    items + weapons

---
+++ TitanShooter/TitanShooter/TitanShooter/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TitanShooter
+{
+    class Weapon : Item
+    {
+        public Weapon(Vector2 pos) : base(pos) { }
+
+        public string WeaponType;
+        public string BulletType;
+
+        public int ClipSize;
+        public int MaxClips;
+
+        public int ReloadTimer;
+        public int ReloadTime; 
+
+        public int ShootDelay;
+        public int ShootDelayCnt;
+
+        public int Weight;
+
+        public int LevelReq;
+        public double Multiplier;
+        public int ItemLevel { get { return ItemLevel; } set { ItemLevel = (int)Math.Round((double)LevelReq * Multiplier, MidpointRounding.AwayFromZero); } }
+        //husk kontroller værdier!
+
+        public int Damage;
+        
+
+        private int myVar;
+
+        
+        
+    }
+}
